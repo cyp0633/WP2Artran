c8a986ac79eb5ddcb924350f2de6119b32076590
cyp0633 <cyp0633@gmail.com>
2023-06-17 22:22:46 +0800

    Get page key

--- internal/trans/convert.go
+++ internal/trans/convert.go
@@ -14,7 +14,29 @@ import (
 )
 
 
-func Convert(comments []fetch.WPComment) {
+func getPageKey(post int) (pageKey string) {
+	// e.g. https://new.example.com/archives/123
+	url := "https://" + conf.Conf.New.Hostname + conf.Conf.Old.PermalinkPrefix + strconv.Itoa(post)
+	// GET web content in url
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	bodyContent, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	// find <meta http-equiv="refresh" content="0; url=https://new.exmaple.com/new-pagekey">
+	regex := regexp.MustCompile(`<meta http-equiv="refresh" content="0; url=https://` + conf.Conf.New.Hostname + `/([^"]+)">`)
+	matches := regex.FindSubmatch(bodyContent)
+	if len(matches) != 2 {
+		panic("no meta refresh found")
+	}
+	pageKey = string(matches[1])
+	if pageKey[0] != '/' {
+		pageKey = "/" + pageKey
+	}
 	return
 }
 
--- internal/trans/convert_test.go
+++ internal/trans/convert_test.go
@@ -1,11 +1,22 @@
 package trans
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/cyp0633/wp-comment-converter/internal/conf"
+)
 
 func TestConvertTime(t *testing.T) {
-	output:=convertTime("2022-12-28T08:30:29")
+	output := convertTime("2022-12-28T08:30:29")
 	t.Log(output)
-	if output!="2022-12-28 08:30:29 +0000 +0000"{
+	if output != "2022-12-28 08:30:29 +0000 +0000" {
 		t.Error("convertTime failed")
 	}
+}
+
+func TestGetPageKey(t *testing.T) {
+	conf.Conf.New.Hostname = "new.example.com"
+	conf.Conf.Old.PermalinkPrefix = "/archives/"
+	output := getPageKey(123)
+	t.Log(output)
 }
